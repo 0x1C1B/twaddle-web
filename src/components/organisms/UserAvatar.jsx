b3d739a653c1739ab6c27d0f267f8e4255ea2a24
Constantin Müller <info@mueller-constantin.de>
2023-08-15 14:00:55 +0200

    feat: add component for generic user avatars

---
+++ src/components/organisms/UserAvatar.jsx
@@ -0,0 +1,48 @@
+import React, {useCallback, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {useNavigate} from 'react-router-dom';
+import Avatar from '../atoms/Avatar';
+import {getUserAvatar} from '../../api/users';
+
+/**
+ * Avatar component for displaying a user's avatar with a fallback to a jdenticon.
+ *
+ * @return {JSX.Element} The avatar component
+ */
+export default function UserAvatar({userId}) {
+  const navigate = useNavigate();
+
+  const [avatar, setAvatar] = React.useState(null);
+
+  const getAvatar = useCallback(async (_userId) => {
+    try {
+      const response = await getUserAvatar(_userId);
+      const blob = new Blob([response.data], {type: response.headers['content-type']});
+      const imageUrl = URL.createObjectURL(blob);
+
+      return imageUrl;
+    } catch (err) {
+      if (err.response && err.response.status === 404) {
+        return null;
+      } else if (err.response && err.response.status === 401) {
+        navigate('/logout');
+      }
+
+      throw err;
+    }
+  }, []);
+
+  useEffect(() => {
+    getAvatar(userId).then((avatar) => setAvatar(avatar));
+  }, [userId, getAvatar]);
+
+  if (avatar) {
+    return <img src={avatar} alt="Avatar" className="w-full h-full object-contain" />;
+  } else {
+    return <Avatar value={userId} />;
+  }
+}
+
+UserAvatar.propTypes = {
+  userId: PropTypes.string.isRequired,
+};
